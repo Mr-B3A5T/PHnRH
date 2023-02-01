20d99615fd31f5b6d7a82d5ad3bcf9245c8bdf2d
Mr-B3A5T <vishwa.i@olak.org>
2023-02-01 18:55:53 +0530

    main and logging changed

--- logging.py
+++ logging.py
@@ -1,6 +1,2 @@
-import logging
-
-logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
-
 def log_event(message):
     logging.info(message)
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import os
-from flask import Flask
-import cv2
-import datetime
-import csv
+import logging
+import logging as log
+
 from login import login
 from dashboard import dashboard
-from video_feed import video_feed_simulation
-from logging import log_information
+from video_stream import video_stream
+from logging import log_event
 
-app = Flask(__name__)
-app.register_blueprint(login)
-app.register_blueprint(dashboard)
+logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
-if __name__ == "__main__":
-    video_feed_simulation()
-    app.run(debug=False)
+# ... Rest of the code
