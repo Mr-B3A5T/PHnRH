3df60155939ff7f7571d5cd3711d450d239d3b32
Mr-B3A5T <vishwa.i@olak.org>
2023-02-01 19:12:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-import logging as log
 
 from login import login
 from dashboard import dashboard
@@ -8,4 +7,16 @@ from logging import log_event
 
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
-# ... Rest of the code
+def main():
+    # Login page
+    is_logged_in = login()
+    
+    if is_logged_in:
+        log_event("User logged in")
+        dashboard()
+        video_stream()
+    else:
+        log_event("Login failed")
+
+if __name__ == "__main__":
+    main()
