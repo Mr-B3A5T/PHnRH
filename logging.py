18882ad0083d8595c7d959577d592698d40b6a40
Mr-B3A5T <vishwa.i@olak.org>
2023-02-02 07:49:23 +0530

    with dashboard.html

--- logging.py
+++ logging.py
@@ -1,2 +1,34 @@
-def log_event(message):
-    logging.info(message)
+import socket
+import csv
+
+def packet_capture():
+    # Create a socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind the socket to a specific address and port
+    server_address = ('localhost', 8000)
+    server_socket.bind(server_address)
+
+    # Listen for incoming connections
+    server_socket.listen(1)
+    print("Packet capture server is running on %s:%s" % server_address)
+
+    # Create a CSV file to store network access packet details
+    with open("/home/osboxes/Desktop/Honeypot/HoneyAccessLog.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["IP Address", "Port", "Connection Type"])
+
+        while True:
+            # Accept incoming connection
+            client_socket, client_address = server_socket.accept()
+            print("Accepted connection from %s:%s" % client_address)
+
+            # Record network access packet details
+            writer.writerow(list(client_address) + ["TCP"])
+
+            # Close the connection
+            client_socket.close()
+
+if __name__ == '__main__':
+    packet_capture()
--- main.py
+++ main.py
@@ -1,44 +1,30 @@
 import socket
-import cv2
-import numpy as np
-import csv
 
-def loop_video(video_path):
-    video = cv2.VideoCapture(video_path)
-
-    while True:
-        ret, frame = video.read()
-
-        if not ret:
-            video = cv2.VideoCapture(video_path)
-            continue
-
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
-
-def cctv_honeypot_server():
+def video_server():
+    # Create a socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind the socket to a specific address and port
     server_address = ('localhost', 8000)
     server_socket.bind(server_address)
-    server_socket.listen(1)
-    print("CCTV honeypot server is running on %s:%s" % server_address)
 
-    video_path = 'fake_cctv_video.mp4'
+    # Listen for incoming connections
+    server_socket.listen(1)
+    print("Video server is running on %s:%s" % server_address)
 
     while True:
+        # Accept incoming connection
         client_socket, client_address = server_socket.accept()
         print("Accepted connection from %s:%s" % client_address)
 
-        with open('network_data.csv', 'a') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['IP Address', 'Port', 'Connection Type', 'Packet Details'])
-            writer.writerow([client_address[0], client_address[1], 'TCP', 'Packet details not recorded'])
+        # Send a fake video stream
+        with open("/home/osboxes/Downloads/cctv.mp4", "rb") as f:
+            video_stream = f.read()
+            client_socket.sendall(video_stream)
 
-        video = loop_video(video_path)
-        client_socket.sendall(video)
-
+        # Close the connection
         client_socket.close()
 
 if __name__ == '__main__':
-    cctv_honeypot_server()
+    video_server()
