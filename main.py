47fe3e352ec84b529b6a008be243c6f63f0e0046
Mr-B3A5T <vishwa.i@olak.org>
2023-02-02 08:17:47 +0530

    demo main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import socket
+import csv
+import time
 
-def video_server():
+def log_data(client_address):
+    with open("/home/osboxes/Desktop/Honeypot/HoneyAccessLog.csv", "a") as log_file:
+        log_writer = csv.writer(log_file)
+        log_writer.writerow([client_address[0], client_address[1], time.time()])
+
+def fake_video_server():
     # Create a socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -11,20 +18,23 @@ def video_server():
 
     # Listen for incoming connections
     server_socket.listen(1)
-    print("Video server is running on %s:%s" % server_address)
+    print("Fake video server is running on %s:%s" % server_address)
 
     while True:
         # Accept incoming connection
         client_socket, client_address = server_socket.accept()
         print("Accepted connection from %s:%s" % client_address)
 
+        # Log the data of the incoming connection
+        log_data(client_address)
+
         # Send a fake video stream
-        with open("/home/osboxes/Downloads/cctv.mp4", "rb") as f:
-            video_stream = f.read()
-            client_socket.sendall(video_stream)
+        with open("/home/osboxes/Downloads/cctv.mp4", "rb") as video_file:
+            video_stream = video_file.read()
+        client_socket.sendall(video_stream)
 
         # Close the connection
         client_socket.close()
 
 if __name__ == '__main__':
-    video_server()
+    fake_video_server()
