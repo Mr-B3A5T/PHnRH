7f9f3ec9da2a9f0f24a7a99dc05f4b7240311e0c
Mr-B3A5T <vishwa.i@olak.org>
2023-02-02 08:28:41 +0530

    demo main

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import socket
 import csv
-import time
 
-def log_data(client_address):
-    with open("/home/osboxes/Desktop/Honeypot/HoneyAccessLog.csv", "a") as log_file:
-        log_writer = csv.writer(log_file)
-        log_writer.writerow([client_address[0], client_address[1], time.time()])
-
-def fake_video_server():
+# Create a fake CCTV server
+def cctv_honeypot_server():
     # Create a socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,23 +13,27 @@ def fake_video_server():
 
     # Listen for incoming connections
     server_socket.listen(1)
-    print("Fake video server is running on %s:%s" % server_address)
+    print("CCTV honeypot server is running on %s:%s" % server_address)
 
     while True:
         # Accept incoming connection
         client_socket, client_address = server_socket.accept()
         print("Accepted connection from %s:%s" % client_address)
+        
+        # Log the connection details
+        with open('/home/osboxes/Desktop/Honeypot/HoneyAccessLog.csv', 'a') as logfile:
+            writer = csv.writer(logfile)
+            writer.writerow([client_address[0], client_address[1], 'TCP'])
 
-        # Log the data of the incoming connection
-        log_data(client_address)
-
-        # Send a fake video stream
-        with open("/home/osboxes/Downloads/cctv.mp4", "rb") as video_file:
+        # Read the video from the local directory
+        with open('/home/osboxes/Downloads/cctv.mp4', 'rb') as video_file:
             video_stream = video_file.read()
+
+        # Send the video stream
         client_socket.sendall(video_stream)
 
         # Close the connection
         client_socket.close()
 
 if __name__ == '__main__':
-    fake_video_server()
+    cctv_honeypot_server()
